2b491c250643d1b22b8eeb74578b37e5916b2b53
dora wu <chiha8888@gmail.com>
2019-10-06 16:54:59 +0800

    add HW02/main2.py

--- HW02/MNIST.py
+++ HW02/MNIST.py
@@ -1,5 +1,37 @@
-from keras.datasets.mnist import load_data
+import numpy as np
 
 def load():
+    '''
+    :return: (60000,784)&(60000)&(10000,784)&(10000) 4 ndarray
+    '''
+    '''
+    from keras.datasets.mnist import load_data
     (train_x,train_y),(test_x,test_y)=load_data()
+    return (train_x,train_y),(test_x,test_y)
+    '''
+    train_x_file=open('train-images.idx3-ubyte','rb')
+    train_y_file=open('train-labels.idx1-ubyte','rb')
+    test_x_file=open('t10k-images.idx3-ubyte','rb')
+    test_y_file=open('t10k-labels.idx1-ubyte','rb')
+
+    #train_x,train_y
+    train_x_file.read(16)
+    train_y_file.read(8)
+    train_x=np.zeros((60000,28*28),dtype='uint8')
+    train_y=np.zeros(60000,dtype='uint8')
+    for i in range(60000):
+        for j in range(28*28):
+            train_x[i,j]=int.from_bytes(train_x_file.read(1),byteorder='big')
+        train_y[i]=int.from_bytes(train_y_file.read(1),byteorder='big')
+
+    #train_y,test_y
+    test_x_file.read(16)
+    test_y_file.read(8)
+    test_x = np.zeros((10000, 28*28),dtype='uint8')
+    test_y=np.zeros(10000,dtype='uint8')
+    for i in range(10000):
+        for j in range(28*28):
+            test_x[i,j] = int.from_bytes(test_x_file.read(1), byteorder='big')
+        test_y[i]=int.from_bytes(test_y_file.read(1),byteorder='big')
+
     return (train_x,train_y),(test_x,test_y)
--- HW02/main.py
+++ HW02/main.py
@@ -8,20 +8,22 @@ import math
 
 if __name__=='__main__':
     (train_x,train_y),(test_x,test_y)=load()
-    train_x=train_x.reshape((train_x.shape[0],train_x.shape[1]*train_x.shape[2]))
-    test_x=test_x.reshape((test_x.shape[0],test_x.shape[1]*test_x.shape[2]))
+    #train_x=train_x.reshape((train_x.shape[0],train_x.shape[1]*train_x.shape[2]))
+    #test_x=test_x.reshape((test_x.shape[0],test_x.shape[1]*test_x.shape[2]))
+    print(train_x.dtype)
+    print(train_y.dtype)
 
     toggle_bar=input('Toggle option (0:discrete / 1:continuous): ')
     #discrete mode or continuous mode(Gaussian)
     if toggle_bar=='0':
         pixvalueProb = get_pixvalueProb_discrete(train_x, train_y)
         prior=get_prior(train_y)
-        test_discrete(1000, pixvalueProb,prior, test_x, test_y)
+        test_discrete(len(test_y), pixvalueProb,prior, test_x, test_y)
         print_imagination_numbers(pixvalueProb,16)
     else:
         pixvalueProb=get_pixvalueProb_continuous(train_x,train_y)
         prior=get_prior(train_y)
-        test_continuous(100,pixvalueProb,prior,test_x,test_y)
+        test_continuous(len(test_y),pixvalueProb,prior,test_x,test_y)
         print_imagination_numbers(pixvalueProb,128)
 
 
